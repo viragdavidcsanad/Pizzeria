88dc1013d1f9118ae84e4ec3624c8b2f25badde5
viragdavidcsanad <viragdavidcsanad@gmail.com>
2022-11-23 02:33:15 +0100

    images and other changes

--- main.js
+++ main.js
@@ -4,17 +4,30 @@ const $hamburgerMenu = document.querySelector(".js_hamburger_menu");
 const $bar1 = document.querySelector(".js_bar1");
 const $bar2 = document.querySelector(".js_bar2");
 const $bar3 = document.querySelector(".js_bar3");
+
+let $navItems = document.querySelectorAll(".js_nav_item");
+let $navItemsClasses = [];
+let $foodMenuButtons = document.querySelectorAll(".js_menu_button");
+let $foodMenuPages = document.querySelectorAll(".js_menu_page");
+let $foodMenuPagesClasses = [];
+
+// ITERATORS
 let i;
-let $navItem = document.querySelectorAll(".js_nav_item");
-$navItem = [...$navItem];
-let $navItemClasses = [];
-for (i in $navItem) {
-  $navItemClasses.push($navItem[i].classList);
+
+// ARRAYS AND CLASSLISTS
+$navItems = [...$navItems];
+for (i in $navItems) {
+  $navItemsClasses.push($navItems[i].classList);
 }
 
-window.onscroll = function () {
-  scrolledHeader();
-};
+$foodMenuButtons = [...$foodMenuButtons];
+
+$foodMenuPages = [...$foodMenuPages];
+for (let menuPage in $foodMenuPages) {
+  $foodMenuPagesClasses.push($foodMenuPages[menuPage].classList);
+}
+
+// FUNCTIONS AND EVENTS
 
 function scrolledHeader() {
   if (
@@ -23,40 +36,28 @@ function scrolledHeader() {
   ) {
     $logo.classList.add("js_scrolled_logo");
     $hamburgerMenu.classList.add("js_scrolled_hamburger_menu");
-    for (i in $navItemClasses) {
-      $navItemClasses[i].add("js_scrolled_nav_item");
+    for (i in $navItemsClasses) {
+      $navItemsClasses[i].add("js_scrolled_nav_item");
     }
   } else {
     $logo.classList.remove("js_scrolled_logo");
     $hamburgerMenu.classList.remove("js_scrolled_hamburger_menu");
-    for (i in $navItemClasses) {
-      $navItemClasses[i].remove("js_scrolled_nav_item");
+    for (i in $navItemsClasses) {
+      $navItemsClasses[i].remove("js_scrolled_nav_item");
     }
-    if ($navBar.classList.contains("js_shifted_nav_bar")) {
-      $navBar.classList.remove("js_shifted_nav_bar");
-    }
-    if ($hamburgerMenu.classList.contains("js_shifted_hamburger_menu")) {
-      $hamburgerMenu.classList.remove("js_shifted_hamburger_menu");
-    }
-    if ($bar1.classList.contains("js_shifted_bar1")) {
-      $bar1.classList.remove("js_shifted_bar1");
-    }
-    if ($bar2.classList.contains("js_shifted_bar2")) {
-      $bar2.classList.remove("js_shifted_bar2");
-    }
-    if ($bar3.classList.contains("js_shifted_bar3")) {
-      $bar3.classList.remove("js_shifted_bar3");
-    }
-    for (i in $navItemClasses) {
-      if ($navItemClasses[i].contains("js_shifted_nav_item")) {
-        $navItemClasses[i].remove("js_shifted_nav_item");
-      }
+    $navBar.classList.remove("js_shifted_nav_bar");
+    $hamburgerMenu.classList.remove("js_shifted_hamburger_menu");
+    $bar1.classList.remove("js_shifted_bar1");
+    $bar2.classList.remove("js_shifted_bar2");
+    $bar3.classList.remove("js_shifted_bar3");
+    for (i in $navItemsClasses) {
+      $navItemsClasses[i].remove("js_shifted_nav_item");
     }
   }
 }
 
-$hamburgerMenu.onclick = function () {
-  menuShifter();
+window.onscroll = function () {
+  scrolledHeader();
 };
 
 function menuShifter() {
@@ -65,7 +66,24 @@ function menuShifter() {
   $bar1.classList.toggle("js_shifted_bar1");
   $bar2.classList.toggle("js_shifted_bar2");
   $bar3.classList.toggle("js_shifted_bar3");
-  for (i in $navItemClasses) {
-    $navItemClasses[i].toggle("js_shifted_nav_item");
+  for (i in $navItemsClasses) {
+    $navItemsClasses[i].toggle("js_shifted_nav_item");
   }
 }
+
+$hamburgerMenu.onclick = function () {
+  menuShifter();
+};
+
+function foodMenuTurner(Event) {
+  for (let onePageClasses of $foodMenuPagesClasses) {
+    onePageClasses.remove("js_active_menu_page");
+  }
+  let eventTargetIndex = $foodMenuButtons.indexOf(Event.target);
+  $foodMenuPagesClasses[eventTargetIndex].add("js_active_menu_page");
+}
+
+for (let button of $foodMenuButtons) {
+  button.addEventListener("click", foodMenuTurner);
+}
+
