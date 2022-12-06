3ce4089069cce2a8457cc6d36f2e2bd8d1fe7ec5
viragdavidcsanad <viragdavidcsanad@gmail.com>
2022-12-07 00:16:01 +0100

    removed pricing.html

--- pricing.js
+++ pricing.js
@@ -1,9 +1,42 @@
 // PRICING TABLES
 
-const $pricingHtml = document.querySelector
-
 function pricingTableMaker(pricingTitles) {
-  let pricingTables = pricingTitles.map((title) => ``).join("");
+  let pricingTables = pricingTitles
+    .map(
+      (title) => `<div class="pricing-table">
+    <a href="#" class="pricing-link">
+      <div class="pricing-content">
+        <h4 class="pricing-heading">${title.name}</h4>
+  
+        <div class="pricing-image-box">
+          <img src="${title.image_link}" alt="pizza picture" class="pricing-image" />
+        </div>
+  
+        <p class="pricing-table-text">${title.ingredients}</p>
+  
+        <h4 class="pricing-sub-heading">Size and Price</h4>
+  
+        <div class="size-and-price">
+          <div class="small size-and-price-row">
+            <span class="small-size size">9"</span>
+                <span class="small-price price">${title.small_price}</span>
+          </div>
+  
+          <div class="large size-and-price-row">
+            <span class="large-size size">14"</span>
+                <span class="large-price price">${title.large_price}</span>
+          </div>
+  
+          <div class="xlarge size-and-price-row">
+            <span class="xlarge-size xlarge-size size">18"</span>
+                <span class="xlarge-price xlarge-price price">${title.x_large_price}</span>
+          </div>
+        </div>
+      </div>
+    </a>
+  </div>`
+    )
+    .join("");
   return pricingTables;
 }
 
