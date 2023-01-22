72d8db16987e03944080641851a963740b5f9f12
viragdavidcsanad <viragdavidcsanad@gmail.com>
2023-01-22 15:11:27 +0100

    in index.html linked menu.css and menu.js, moved the contain of menu.js from main.js to menu.js, contain of menu.css moved from style.css to menu.css, in main.js 'for' loops transformed into map functions, hamburger menu color changes

--- main.js
+++ main.js
@@ -5,28 +5,16 @@ const $bar1 = document.querySelector(".js_bar1");
 const $bar2 = document.querySelector(".js_bar2");
 const $bar3 = document.querySelector(".js_bar3");
 const $navLinks = document.querySelectorAll(".js_nav_link");
-
 let $navItems = document.querySelectorAll(".js_nav_item");
 let $navItemsClasses = [];
-let $foodMenuButtons = document.querySelectorAll(".js_menu_button");
-let $foodMenuPages = document.querySelectorAll(".js_menu_page");
-let $foodMenuPagesClasses = [];
-let i;
 
-// ==================== ARRAYS AND CLASSLISTS ========================
-$navItems = [...$navItems];
-for (i in $navItems) {
-  $navItemsClasses.push($navItems[i].classList);
-}
-$foodMenuButtons = [...$foodMenuButtons];
-$foodMenuPages = [...$foodMenuPages];
-for (let menuPage in $foodMenuPages) {
-  $foodMenuPagesClasses.push($foodMenuPages[menuPage].classList);
-}
 // *********************************************************************
 // ======================= FUNCTIONS AND EVENTS ========================
 // *********************************************************************
 // ========== HEADER SCROLL EFFECT ON LOGO AND NAVIGATION ==============
+
+$navItems = [...$navItems];
+$navItems.map((item) => $navItemsClasses.push(item.classList));
 
 function scrolledHeader() {
   if (
@@ -36,24 +24,20 @@ function scrolledHeader() {
     $logo.classList.add("scrolled-logo");
     $navBar.classList.add("scrolled-nav-bar");
     $hamburgerMenu.classList.add("scrolled-hamburger-menu");
-    for (i in $navItemsClasses) {
-      $navItemsClasses[i].add("scrolled-nav-item");
-    }
+    $navItemsClasses.map((classlist) => classlist.add("scrolled-nav-item"));
   } else {
     $logo.classList.remove("scrolled-logo");
     $navBar.classList.remove("scrolled-nav-bar");
     $hamburgerMenu.classList.remove("scrolled-hamburger-menu");
-    for (i in $navItemsClasses) {
-      $navItemsClasses[i].remove("scrolled-nav-item");
-    }
+    $navItemsClasses.map((classlist) => {
+      classlist.remove("scrolled-nav-item");
+      classlist.remove("shifted-nav-item");
+    });
     $navBar.classList.remove("shifted-nav-bar");
     $hamburgerMenu.classList.remove("shifted-hamburger-menu");
     $bar1.classList.remove("shifted-bar1");
     $bar2.classList.remove("shifted-bar2");
     $bar3.classList.remove("shifted-bar3");
-    for (i in $navItemsClasses) {
-      $navItemsClasses[i].remove("shifted-nav-item");
-    }
   }
 }
 
@@ -69,46 +53,9 @@ function menuShifter() {
   $bar1.classList.toggle("shifted-bar1");
   $bar2.classList.toggle("shifted-bar2");
   $bar3.classList.toggle("shifted-bar3");
-  for (i in $navItemsClasses) {
-    $navItemsClasses[i].toggle("shifted-nav-item");
-  }
+  $navItemsClasses.map((classlist) => classlist.toggle("shifted-nav-item"));
 }
 
 $hamburgerMenu.onclick = function () {
   menuShifter();
 };
-
-// ======================== FOOD AND DRINK MENU ============================
-
-function foodMenuHeight() {
-  let menuHeight = document.querySelector(".js_active_menu_page").clientHeight;
-  document
-    .querySelector(".js_menu_pages")
-    .setAttribute("style", `height: ${menuHeight + 60}px`);
-}
-
-foodMenuHeight();
-
-function foodMenuTurner(Event) {
-  let eventTargetIndex = $foodMenuButtons.indexOf(Event.target);
-  for (let pageIndex in $foodMenuPagesClasses) {
-    $foodMenuPagesClasses[pageIndex].remove(
-      "js_active_menu_page",
-      "active-menu-page",
-      "after-menu-page",
-      "before-menu-page"
-    );
-    if (pageIndex < eventTargetIndex) {
-      $foodMenuPagesClasses[pageIndex].add("before-menu-page");
-    } else if (pageIndex > eventTargetIndex) {
-      $foodMenuPagesClasses[pageIndex].add("after-menu-page");
-    } else {
-      $foodMenuPagesClasses[pageIndex].add("js_active_menu_page active-menu-page");
-    }
-  }
-  foodMenuHeight();
-}
-
-for (let button of $foodMenuButtons) {
-  button.addEventListener("click", foodMenuTurner);
-}
---
+++ menu/menu.js
@@ -0,0 +1,44 @@
+let $foodMenuButtons = document.querySelectorAll(".js_menu_button");
+let $foodMenuPages = document.querySelectorAll(".js_menu_page");
+let $foodMenuPagesClasses = [];
+$foodMenuButtons = [...$foodMenuButtons];
+$foodMenuPages = [...$foodMenuPages];
+for (let menuPage in $foodMenuPages) {
+  $foodMenuPagesClasses.push($foodMenuPages[menuPage].classList);
+}
+
+function foodMenuHeight() {
+  let menuHeight = document.querySelector(".js_active_menu_page").clientHeight;
+  document
+    .querySelector(".js_menu_pages")
+    .setAttribute("style", `height: ${menuHeight + 60}px`);
+}
+
+foodMenuHeight();
+
+function foodMenuTurner(Event) {
+  let eventTargetIndex = $foodMenuButtons.indexOf(Event.target);
+  for (let pageIndex in $foodMenuPagesClasses) {
+    $foodMenuPagesClasses[pageIndex].remove(
+      "js_active_menu_page",
+      "active-menu-page",
+      "after-menu-page",
+      "before-menu-page"
+    );
+    if (pageIndex < eventTargetIndex) {
+      $foodMenuPagesClasses[pageIndex].add("before-menu-page");
+    } else if (pageIndex > eventTargetIndex) {
+      $foodMenuPagesClasses[pageIndex].add("after-menu-page");
+    } else {
+      $foodMenuPagesClasses[pageIndex].add(
+        "js_active_menu_page",
+        "active-menu-page"
+      );
+    }
+  }
+  foodMenuHeight();
+}
+
+for (let button of $foodMenuButtons) {
+  button.addEventListener("click", foodMenuTurner);
+}
