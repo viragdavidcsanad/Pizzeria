fb84f698d5ca13779844c6ce4018cc768f5ed086
viragdavidcsanad <viragdavidcsanad@gmail.com>
2023-03-26 22:36:59 +0200

    pricing.js: formatted template literals, portions modification in the rendering

--- pricing/pricing.js
+++ pricing/pricing.js
@@ -8,40 +8,47 @@ const linksOfProducts = [
 const currencyLink = "./data/currency.json";
 
 const portionAndPriceRow = (title, dataArray, currency) => {
-  const portionsObject = dataArray.filter((data) => data.portions)[0].portions;
-  let portionAndPriceRows = [];
+  const portionsCentralObject = dataArray.filter((data) => data.portions)[0]
+    .portions;
+  const portionsProductObject = title.portions;
+  const portionsObject = () => {
+    if (portionsProductObject === undefined) {
+      return portionsCentralObject;
+    } else {
+      return portionsProductObject;
+    }
+  };
+  let portionAndPriceRows = "";
   for (let portion in title.prices) {
-    portionAndPriceRows.push(
-      `<div class="portion-and-price-row">
-        <span class="portion">${portion}</span>
-        <span class="portion-content">${portionsObject[portion]}</span>
-        <span class="price">${currency}${title.prices[portion]}</span>
-    </div>`
-    );
+    portionAndPriceRows += `<div class="portion-and-price-row">
+                              <span class="portion">${portion}</span>
+                              <span class="portion-content">${portionsObject()[portion]}</span>
+                              <span class="price">${currency}${title.prices[portion]}</span>
+                            </div>`;
   }
-  return portionAndPriceRows.join("");
+  return portionAndPriceRows;
 };
 
 const pricingTableMaker = (pricingTitles, dataArray, currency) => {
   let pricingTables = pricingTitles
     .map(
       (title) => `<div class="pricing-table">
-    <a href="#" class="pricing-link">
-      <div class="pricing-content">
-        <h4 class="pricing-heading">${title.name}</h4>
-        <div class="pricing-image-box">
-          <img src="${
-            title.image_link
-          }" alt="pizza picture" class="pricing-image" />
-        </div>
-        <p class="pricing-table-text">${title.ingredients}</p>
-        <h4 class="pricing-sub-heading">Prices</h4>
-        <div class="portion-and-price">
-        ${portionAndPriceRow(title, dataArray, currency)}    
-        </div>
-      </div>
-    </a>
-  </div>`
+                    <a href="./product_page/product_page.html" class="pricing-link">
+                      <div class="pricing-content">
+                        <h4 class="pricing-heading">${title.name}</h4>
+                        <div class="pricing-image-box">
+                          <img src="${
+                            title.image_link
+                          }" alt="pizza picture" class="pricing-image" />
+                        </div>
+                        <p class="pricing-table-text">${title.ingredients}</p>
+                        <h4 class="pricing-sub-heading">Prices</h4>
+                        <div class="portion-and-price">
+                        ${portionAndPriceRow(title, dataArray, currency)}    
+                        </div>
+                      </div>
+                    </a>
+                  </div>`
     )
     .join("");
   return pricingTables;
