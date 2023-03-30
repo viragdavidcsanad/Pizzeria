a5e5f80a78d7bebf6e3b7228983ed32800e33824
viragdavidcsanad <viragdavidcsanad@gmail.com>
2023-03-30 23:21:17 +0200

    product_page.js: product id import

--- product_page/product_page.js
+++ product_page/product_page.js
@@ -1,16 +1,18 @@
+import { productId } from "../pricing/pricing.js";
+
+console.log(productId);
+
 const linkArray = [
-  "./data/pizza.json",
-  "./data/pasta.json",
-  "./data/salad.json",
-  "./data/drink.json",
+  "../data/pizza.json",
+  "../data/pasta.json",
+  "../data/salad.json",
+  "../data/drink.json",
 ];
 
 const productPageFetch = () => {
   linkArray.map((link) =>
     fetch(link)
       .then((data) => data.json())
-      .then((jsonData) => {
-
-	  })
+      .then((jsonData) => {})
   );
 };
