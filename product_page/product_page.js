394d8d1d003e2b5f7c208e6052df95f15e6c08c1
viragdavidcsanad <viragdavidcsanad@gmail.com>
2023-04-10 23:17:07 +0200

    product_page.js: links to json files and fetch function into comment

--- product_page/product_page.js
+++ product_page/product_page.js
@@ -1,18 +1,37 @@
-import { productId } from "../pricing/pricing.js";
+// import { productPageEvent } from "../pricing/pricing.js";
 
-console.log(productId);
+// await import("../pricing/pricing.js").then(({ pricingClickEvent }) =>
+//   console.log(pricingClickEvent)
+// );
 
-const linkArray = [
-  "../data/pizza.json",
-  "../data/pasta.json",
-  "../data/salad.json",
-  "../data/drink.json",
-];
+// const linkArray = [
+//   "../data/pizza.json",
+//   "../data/pasta.json",
+//   "../data/salad.json",
+//   "../data/drink.json",
+// ];
 
-const productPageFetch = () => {
-  linkArray.map((link) =>
-    fetch(link)
-      .then((data) => data.json())
-      .then((jsonData) => {})
-  );
+// const productPageFetch = () => {
+//   linkArray.map((link) =>
+//     fetch(link)
+//       .then((data) => data.json())
+//       .then((jsonData) => {
+//         jsonData.products.filter(
+//           (product) => product.id === productPageEvent.currentTarget.id
+//         );
+//       })
+//   );
+// };
+
+const $productPage = document.querySelector(".js_product_page");
+
+const focusEvent = $productPage.addEventListener("focus", fillProductData)
+
+
+const $pricingLinks = () => {
+  if (document.querySelector(".js_product_page:focus")) {
+    return [...document.querySelectorAll(".js_pricing_link")];
+  }
 };
+
+console.log($pricingLinks());
