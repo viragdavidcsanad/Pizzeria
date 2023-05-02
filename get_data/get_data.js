212ae5f800797b88629f5fcfd7df6cf7183e01c4
viragdavidcsanad <viragdavidcsanad@gmail.com>
2023-05-02 23:29:18 +0200

    get_data.js: json data fetch and export default await

--- get_data/get_data.js
+++ get_data/get_data.js
@@ -5,14 +5,31 @@ const dataLinks = [
   "../data/pizza.json",
   "../data/pasta.json",
   "../data/salad.json",
-  "../dessert.json",
+  "../data/dessert.json",
   "../data/drink.json",
 ];
 
-const link = "../data/pizza.json";
+const getData = async (dataLinks) => {
+  const dataArray = [];
+  await dataLinks.map(
+    async (link) =>
+      await fetch(link)
+        .then((data) => data.json())
+        .then((jsonData) => dataArray.push(jsonData))
+        .catch((error) => console.table("COUGHT ERROR:", error))
+  );
+  return dataArray;
+};
 
-const jsonData = async () => Promise.allSettled fetch(link)
-  .then((data) => data.json())
-  .catch((error) => console.table(error));
+// async function getData(dataLinks) {
+//   const dataArray = [];
+//   for (let link of dataLinks) {
+//     await fetch(link)
+//       .then((data) => data.json())
+//       .then((jsonData) => dataArray.push(jsonData))
+//       .catch((error) => console.table("COUGHT ERROR:", error));
+//   }
+//   return dataArray;
+// }
 
-// export default await jsonData;
+export default await getData(dataLinks);
