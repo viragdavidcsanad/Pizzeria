c754bebbcecf3236b26f0acaf5eaf9c7d0566e3e
viragdavidcsanad <viragdavidcsanad@gmail.com>
2023-05-15 20:09:47 +0200

    get_data.json: new categries.json link, fetched data array => object

--- get_data/get_data.js
+++ get_data/get_data.js
@@ -1,23 +1,26 @@
 const dataLinks = [
-  "../data/currency.json",
-  "../data/appetizer.json",
-  "../data/soup.json",
-  "../data/pizza.json",
-  "../data/pasta.json",
-  "../data/salad.json",
-  "../data/dessert.json",
-  "../data/drink.json",
+  "./data/currency.json",
+  "./data/categories.json",
+  "./data/appetizer.json",
+  "./data/soup.json",
+  "./data/pizza.json",
+  "./data/pasta.json",
+  "./data/salad.json",
+  "./data/dessert.json",
+  "./data/drink.json",
 ];
 
 async function getData(dataLinks) {
-  const dataArray = [];
+  let dataObject = {};
   for (let link of dataLinks) {
     await fetch(link)
       .then((data) => data.json())
-      .then((jsonData) => dataArray.push(jsonData))
+      .then((jsonData) => {
+        dataObject = { ...dataObject, ...jsonData };
+      })
       .catch((error) => console.table("COUGHT ERROR:", error));
   }
-  return dataArray;
+  return dataObject;
 }
 
 export default await getData(dataLinks);
