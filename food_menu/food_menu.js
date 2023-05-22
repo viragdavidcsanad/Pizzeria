ec84f27f8d0285a9d3b288f6455d8e0d2834a253
viragdavidcsanad <viragdavidcsanad@gmail.com>
2023-05-22 21:14:43 +0200

    food_menu.js: new data construction is rendered

--- food_menu/food_menu.js
+++ food_menu/food_menu.js
@@ -1,11 +1,10 @@
-const foodLinks = [
-  "./data/pizza.json",
-  "./data/pasta.json",
-  "./data/salad.json",
-  "./data/drink.json",
-];
+import allData from "../get_data/get_data.js";
+const selectors = allData.selectors;
+const filteredCategories = allData.categories.filter(
+  (category) => allData[category].products
+);
 
-const selectorLink = "./data/selector.json"
+const selectorLink = "./data/selector.json";
 
 let $foodMenuHeadingsArray;
 const $headingsContainer = document.querySelector(".js_food_menu_headings");
@@ -17,9 +16,30 @@ const $foodMenuSelector = document.querySelector(
   ".js_invisible_food_menu_selector"
 );
 
-const foodFilter = (jsonData) => {
+const foodMenuHeight = () => {
+  const $pagesArray = [...$pagesContainer.children];
+  const $activePage = document.querySelector(".js_active_menu_page");
+  const menuHeight = () => {
+    if ($activePage === null) {
+      return $pagesArray.reduce(
+        (accumulator, element) => accumulator + element.offsetHeight,
+        0
+      );
+    } else return $activePage.offsetHeight;
+  };
+  return $pagesContainer.setAttribute(
+    "style",
+    `height: ${menuHeight() / 10 + 6}rem`
+  );
+};
+
+const resizeEvent = () => window.addEventListener("resize", foodMenuHeight);
+
+resizeEvent();
+
+const foodFilter = (currentCategory) => {
   let filteredFood = [];
-  const products = jsonData.filter((data) => data.products)[0].products;
+  const products = currentCategory.products;
   const selectorValue = $foodMenuSelector.value;
   const drink = products.filter(
     (product) => product.product_category === "Drink"
@@ -58,36 +78,35 @@ const renderFoodMenuProducts = (jsonData, subcategory) => {
   return foodMenuProducts;
 };
 
-const renderSubcategories = (jsonData) => {
-  const subcategoryArray = jsonData.filter((data) => data.subcategory)[0]
-    .subcategory;
+const renderSubcategories = (currentCategory) => {
+  const subcategoryArray = currentCategory.subcategory;
   let subcategories = "";
   subcategoryArray.map((subcategory) => {
-    if (renderFoodMenuProducts(jsonData, subcategory) === "") {
+    if (renderFoodMenuProducts(currentCategory, subcategory) === "") {
       return;
     } else {
       subcategories += `<div class="food-menu-subcategory-table js_food_menu_subcategory_table">
                           <h4 class="food-menu-subcategory">${subcategory}</h4>
-                          ${renderFoodMenuProducts(jsonData, subcategory)}
+                          ${renderFoodMenuProducts(currentCategory, subcategory)}
                         </div>`;
     }
   });
   return subcategories;
 };
 
-const renderHeadingsAndPages = (jsonData, index) => {
-  const category = jsonData.filter((data) => data.category)[0].category;
+const renderHeadingsAndPages = (currentCategory, index) => {
+  const category = currentCategory.category;
   if (index === 0) {
     $headingsContainer.innerHTML = "";
     $pagesContainer.innerHTML = "";
   }
-  if (renderSubcategories(jsonData) === "") {
+  if (renderSubcategories(currentCategory) === "") {
     return;
   } else {
     $headingsContainer.innerHTML += `<button class="food-menu-heading ${category} js_food_menu_heading">${category}</button>`;
     $pagesContainer.innerHTML += `<div class="food-menu-page ${category} js_food_menu_page">
                                     <h3 class="food-menu-category">${category}</h3>
-                                    ${renderSubcategories(jsonData)}
+                                    ${renderSubcategories(currentCategory)}
                                   </div>`;
   }
   foodMenuHeight();
@@ -138,9 +157,9 @@ const foodMenuTurner = (Event) => {
   foodMenuHeight();
 };
 
-const foodMenuChangeEvent = (jsonData, index) => {
+const foodMenuChangeEvent = (currentCategory, index) => {
   $foodMenuSelector.addEventListener("change", () => {
-    renderHeadingsAndPages(jsonData, index);
+    renderHeadingsAndPages(currentCategory, index);
   });
 };
 
@@ -196,22 +215,17 @@ const selectorClickEvent = ($visibleSelector, $optionsArray) => {
   );
 };
 
-const fetchFoodMenu = () => {
-  foodLinks.map((link, index) =>
-    fetch(link)
-      .then((rawData) => rawData.json())
-      .then((jsonData) => {
-          renderHeadingsAndPages(jsonData);
-          turnerClickEvent();
-          foodMenuChangeEvent(jsonData, index);
-      })
-  );
+const renderFoodMenu = () => {
+  filteredCategories.map((filteredCategory, index) => {
+    const currentCategory = allData[filteredCategory];
+    renderHeadingsAndPages(currentCategory, index);
+    turnerClickEvent();
+    foodMenuChangeEvent(currentCategory, index);
+  });
 };
 
-const renderFoodMenuSelector = async () => {
-  const rawData = await fetch(selectorLink);
-  const jsonData = await rawData.json();
-  const selectors = jsonData.filter((data) => data.selectors)[0].selectors;
+const renderFoodMenuSelector = () => {
+  const selectors = allData.selectors;
   for (let selector in selectors) {
     $foodMenuSelector.innerHTML += `<option value="${selector}" class="food-menu-invisible-selector-option js_food_menu_invisible_selector_option">
                                       ${selectors[selector]}
@@ -238,30 +252,9 @@ const renderFoodMenuSelector = async () => {
   const $optionsArray = [
     ...document.querySelectorAll(".js_food_menu_visible_selector_option"),
   ];
-  fetchFoodMenu();
+  renderFoodMenu();
   selectorClickEvent($visibleSelector, $optionsArray);
   selectorBlurEvent($visibleSelector, $optionsArray);
 };
 
 renderFoodMenuSelector();
-
-const foodMenuHeight = () => {
-  const $pagesArray = [...$pagesContainer.children];
-  const $activePage = document.querySelector(".js_active_menu_page");
-  const menuHeight = () => {
-    if ($activePage === null) {
-      return $pagesArray.reduce(
-        (accumulator, element) => accumulator + element.offsetHeight,
-        0
-      );
-    } else return $activePage.offsetHeight;
-  };
-  return $pagesContainer.setAttribute(
-    "style",
-    `height: ${menuHeight() / 10 + 6}rem`
-  );
-};
-
-const resizeEvent = () => window.addEventListener("resize", foodMenuHeight);
-
-resizeEvent();
