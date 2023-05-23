0f7fa4d4a5e7cb438cbe8750fcd950e1cc3e8738
viragdavidcsanad <viragdavidcsanad@gmail.com>
2023-05-23 21:52:53 +0200

    selected product id access

--- product_page/product_page.js
+++ product_page/product_page.js
@@ -1,8 +1,13 @@
 import allData from "../get_data/get_data.js";
 
 const $productPage = document.querySelector(".js_product_page");
-const fillProductData = () => console.log($productPage.dataset.id);
+const fillProductData = () => {
+  const $selectedProduct = document.querySelector(".js_selected_product");
+  console.log($selectedProduct.dataset.id);
+};
 const focusEvent = () =>
   $productPage.addEventListener("focus", fillProductData);
+
+focusEvent();
 
 // console.log($pricingLinks());
