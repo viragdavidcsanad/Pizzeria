d70f0abc1d5d183d8343455e2071878a9f9102f8
viragdavidcsanad <viragdavidcsanad@gmail.com>
2023-05-23 21:44:52 +0200

    product page reorganization

--- product_page/product_page.js
+++ product_page/product_page.js
@@ -1,25 +1,8 @@
-const productLinks = [
-  "../data/pizza.json",
-  "../data/pasta.json",
-  "../data/salad.json",
-  "../data/drink.json",
-];
+import allData from "../get_data/get_data.js";
 
 const $productPage = document.querySelector(".js_product_page");
-const focusEvent = () => $productPage.addEventListener("focus", fillProductData)
-
-const productPageFetch = () => {
-  productLinks.map((link) =>
-    fetch(link)
-      .then((data) => data.json())
-      .then((jsonData) => {
-        jsonData.products.filter(
-          (product) => product.id === productPageEvent.currentTarget.id
-        );
-      })
-  );
-};
-
-
+const fillProductData = () => console.log($productPage.dataset.id);
+const focusEvent = () =>
+  $productPage.addEventListener("focus", fillProductData);
 
 // console.log($pricingLinks());
