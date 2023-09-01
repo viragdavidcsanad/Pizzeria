d46b3e90b22b0499fcf27041b5a7796cfcd8f6f6
David Virag <viragdavidcsanad@gmail.com>
2023-09-02 01:15:16 +0200

    template literal format modification

--- food_menu/food_menu.js
+++ food_menu/food_menu.js
@@ -105,7 +105,9 @@ const renderHeadingsAndPages = (currentCategory, index) => {
   if (renderSubcategories(currentCategory) === "") {
     return;
   } else {
-    $headingsContainer.innerHTML += `<button class="food-menu-heading ${category} js_food_menu_heading">${category}</button>`;
+    $headingsContainer.innerHTML += `<button class="food-menu-heading ${category}js_food_menu_heading">
+                                       ${category}
+                                     </button>`;
     $pagesContainer.innerHTML += `<div class="food-menu-page ${category} js_food_menu_page">
                                     <h3 class="food-menu-category">${category}</h3>
                                     ${renderSubcategories(currentCategory)}
